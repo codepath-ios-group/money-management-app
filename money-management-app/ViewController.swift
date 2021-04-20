e224eac36668afd729ad207b0b75e4f90a33f24e
georgemelek <melekgeorge@outlook.com>
2021-04-20 15:29:49 -0400

    Initial Commit

---
+++ money-management-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  money-management-app
+//
+//  Created by George Melek on 4/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
