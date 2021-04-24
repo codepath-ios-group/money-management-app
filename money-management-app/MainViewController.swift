7f7831dd336b186bf6bd43e3d4fef2927aa30f58
Jason Chan <jasonchan.newyork@gmail.com>
2021-04-23 22:56:46 -0400

    finished login

---
+++ money-management-app/MainViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MainViewController.swift
+//  money-management-app
+//
+//  Created by Jason  Chan on 4/23/21.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
